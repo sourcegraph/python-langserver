9d17d11215613d138cbf0c0ea0cd78ca40f5ab7f
Renfred Harper <renfredxh@gmail.com>
2016-11-10 14:44:56 -0800

    Refactor code, stub tcp implmentation

--- langserver/langserver.py
+++ langserver/langserver.py
@@ -1,53 +1,115 @@
 import sys
 import json
 import jedi
-
-class JSONRPC2Error(Exception):
-    pass
+import argparse
+from abc import ABC, abstractmethod
 
 def log(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
-def path_from_uri(uri):
-    _, path = uri.split("file://", 1)
-    return path
+class JSONRPC2Error(Exception):
+    pass
 
-def read_header_content_length(line):
-    if len(line) < 2 or line[-2:] != "\r\n":
-        raise JSONRPC2Error("Line endings must be \\r\\n")
-    if line.startswith("Content-Length: "):
-        _, value = line.split("Content-Length: ")
-        value = value.strip()
-        try:
-            return int(value)
-        except ValueError:
-            raise JSONRPC2Error("Invalid Content-Length header: {}".format(value))
+class ReadWriter(ABC):
+    @abstractmethod
+    def readline(self, size):
+        pass
 
-def write_response(id, result):
-    body = {
-        "jsonrpc": "2.0",
-        "id": id,
-        "result": result,
-    }
-    body = json.dumps(body, separators=(",", ":"))
-    content_length = len(body)
-    response = (
-        "Content-Length: {}\r\n"
-        "Content-Type: application/vscode-jsonrpc; charset=utf8\r\n\r\n"
-        "{}".format(content_length, body))
-    sys.stdout.write(response)
-    sys.stdout.flush()
-    log("RESPONSE: ", response)
+    @abstractmethod
+    def read(self, size):
+        pass
 
-def read_messages():
-    while True:
-        line = sys.stdin.readline()
-        length = read_header_content_length(line)
-        body = sys.stdin.read(length+2) # TODO(renfred): why the off-by-two?
-        request = json.loads(body)
-        log("REQUEST: ", length, request)
+    @abstractmethod
+    def write(self, size):
+        pass
+
+class StdIOReadWriter(ReadWriter):
+    def readline(self, *args):
+        return sys.stdin.readline(*args)
+
+    def read(self, *args):
+        return sys.stdin.read(*args)
+
+    def write(self, out):
+        sys.stdout.write(out)
+        sys.stdout.flush()
+
+class SocketReadWriter(ReadWriter):
+    def __init__(self, socket):
+        self.socket = socket
+        self.buffer = ""
+
+    def readline(self, size=None):
+        buffering = True
+        while buffering:
+            if "\n" in self.buffer:
+                line, self.buffer = buffer.split("\n", 1)
+                if size:
+                    line, self.buffer = line[:size], self.buffer + line[size:]
+                return line
+            else:
+                next = socket.recv(4096)
+                if not next:
+                    buffering = False
+                else:
+                    self.buffer += more
+
+    def read(self, size):
+        if len(self.buffer) >= size:
+            out, self.buffer = self.buffer[:size], self.buffer[size:]
+        recv_size = size - len(self.buffer)
+        next = socket.recv(recv_size)
+        out, self.buffer = self.buffer + next, self.buffer
+        return out
+
+class JSONRPC2Server:
+    def __init__(self, conn=None):
+        self.conn = conn
+
+    def handle(self, id, request):
+        pass
+
+    def _read_header_content_length(self, line):
+        if len(line) < 2 or line[-2:] != "\r\n":
+            raise JSONRPC2Error("Line endings must be \\r\\n")
+        if line.startswith("Content-Length: "):
+            _, value = line.split("Content-Length: ")
+            value = value.strip()
+            try:
+                return int(value)
+            except ValueError:
+                raise JSONRPC2Error("Invalid Content-Length header: {}".format(value))
+
+    def write_response(self, id, result):
+        body = {
+            "jsonrpc": "2.0",
+            "id": id,
+            "result": result,
+        }
+        body = json.dumps(body, separators=(",", ":"))
+        content_length = len(body)
+        response = (
+            "Content-Length: {}\r\n"
+            "Content-Type: application/vscode-jsonrpc; charset=utf8\r\n\r\n"
+            "{}".format(content_length, body))
+        self.conn.write(response)
+        log("RESPONSE: ", response)
+
+    def serve(self):
+        while True:
+            line = self.conn.readline()
+            length = self._read_header_content_length(line)
+            body = self.conn.read(length+2) # TODO(renfred): why the off-by-two?
+            request = json.loads(body)
+            self.handle(id, request)
+
+class LangServer(JSONRPC2Server):
+    def handle(self, id, request):
+        log("REQUEST: ", id, request)
+        resp = ""
+
         if request["method"] == "initialize":
-            write_response(request["id"], {
+            resp = {
                 "capabilities": {
                     # "textDocumentSync": 1,
                     "hoverProvider": True,
@@ -55,119 +117,133 @@ def read_messages():
                     "referencesProvider": True,
                     # "workspaceSymbolProvider": True TODO
                 }
-            })
-
-        elif request["method"] == "textDocument/hover":
-            write_response(request["id"], handle_hover(request))
-
-        elif request["method"] == "textDocument/definition":
-            write_response(request["id"], handle_definition(request))
-
-        elif request["method"] == "textDocument/references":
-            write_response(request["id"], handle_references(request))
-
-def handle_hover(request):
-    params = request["params"]
-    pos = params["position"]
-    path = path_from_uri(params["textDocument"]["uri"])
-    source = open(path).read()
-    if len(source.split("\n")[pos["line"]]) < pos["character"]:
-        return {}
-    script = jedi.api.Script(path=path, source=source, line=pos["line"]+1,
-                             column=pos["character"])
-
-    defs, error = [], None
-    try:
-        defs = script.goto_definitions()
-    except Exception as e:
-        # TODO return these errors using JSONRPC properly. Doing it this way
-        # initially for debugging purposes.
-        error = "ERROR {}: {}".format(type(e), e)
-    d = defs[0] if len(defs) > 0 else None
-
-    # TODO(renfred): better failure mode
-    if d is None:
-        value = error or "404 Not Found"
-        return {
-            "contents": [{"language": "markdown", "value": value}],
-        }
-
-    hover_info = d.docstring() or d.description
-    return {
-        # TODO(renfred): convert reStructuredText docstrings to markdown.
-        "contents": [{"language": "markdown", "value": hover_info}],
-    }
-
-def handle_definition(request):
-    params = request["params"]
-    pos = params["position"]
-    path = path_from_uri(params["textDocument"]["uri"])
-    source = open(path).read()
-    if len(source.split("\n")[pos["line"]]) < pos["character"]:
-        return {}
-    script = jedi.api.Script(path=path, source=source, line=pos["line"]+1,
-                             column=pos["character"])
-
-    defs = script.goto_definitions()
-    assigns = script.goto_assignments()
-    d = None
-    if len(defs) > 0:
-        d = defs[0]
-    elif len(assigns) > 0:
-        # TODO(renfred): figure out if this works in all cases.
-        d = assigns[0]
-    if d is None: return {}
-
-    return {
-        "uri": "file://" + d.module_path,
-        "range": {
-            "start": {
-                "line": d.line-1,
-                "character": d.column,
-            },
-            "end": {
-                "line": d.line-1,
-                "character": d.column,
             }
+        elif request["method"] == "textDocument/hover":
+            resp = self.serve_hover(request)
+        elif request["method"] == "textDocument/definition":
+            resp = self.serve_definition(request)
+        elif request["method"] == "textDocument/references":
+            resp = self.serve_references(request)
+
+        if resp:
+            self.write_response(request["id"], resp)
+
+    def path_from_uri(self, uri):
+        _, path = uri.split("file://", 1)
+        return path
+
+    def serve_hover(self, request):
+        params = request["params"]
+        pos = params["position"]
+        path = self.path_from_uri(params["textDocument"]["uri"])
+        source = open(path).read()
+        if len(source.split("\n")[pos["line"]]) < pos["character"]:
+            return {}
+        script = jedi.api.Script(path=path, source=source, line=pos["line"]+1,
+                                 column=pos["character"])
+
+        defs, error = [], None
+        try:
+            defs = script.goto_definitions()
+        except Exception as e:
+            # TODO return these errors using JSONRPC properly. Doing it this way
+            # initially for debugging purposes.
+            error = "ERROR {}: {}".format(type(e), e)
+        d = defs[0] if len(defs) > 0 else None
+
+        # TODO(renfred): better failure mode
+        if d is None:
+            value = error or "404 Not Found"
+            return {
+                "contents": [{"language": "markdown", "value": value}],
+            }
+
+        hover_info = d.docstring() or d.description
+        return {
+            # TODO(renfred): convert reStructuredText docstrings to markdown.
+            "contents": [{"language": "markdown", "value": hover_info}],
         }
-    }
 
-def handle_references(request):
-    params = request["params"]
-    pos = params["position"]
-    path = path_from_uri(params["textDocument"]["uri"])
-    source = open(path).read()
-    if len(source.split("\n")[pos["line"]]) < pos["character"]:
-        return {}
-    script = jedi.api.Script(path=path, source=source, line=pos["line"]+1,
-                             column=pos["character"])
+    def serve_definition(self, request):
+        params = request["params"]
+        pos = params["position"]
+        path = self.path_from_uri(params["textDocument"]["uri"])
+        source = open(path).read()
+        if len(source.split("\n")[pos["line"]]) < pos["character"]:
+            return {}
+        script = jedi.api.Script(path=path, source=source, line=pos["line"]+1,
+                                 column=pos["character"])
 
-    usages = script.usages()
-    if len(usages) == 0:
-        return {}
+        defs = script.goto_definitions()
+        assigns = script.goto_assignments()
+        d = None
+        if len(defs) > 0:
+            d = defs[0]
+        elif len(assigns) > 0:
+            # TODO(renfred): figure out if this works in all cases.
+            d = assigns[0]
+        if d is None: return {}
 
-    refs = []
-    for u in usages:
-        if u.is_definition():
-            continue
-        refs.append({
-            "uri": "file://" + u.module_path,
+        return {
+            "uri": "file://" + d.module_path,
             "range": {
                 "start": {
-                    "line": u.line-1,
-                    "character": u.column,
+                    "line": d.line-1,
+                    "character": d.column,
                 },
                 "end": {
-                    "line": u.line-1,
-                    "character": u.column,
+                    "line": d.line-1,
+                    "character": d.column,
                 }
             }
-        })
-    return refs
+        }
+
+    def serve_references(self, request):
+        params = request["params"]
+        pos = params["position"]
+        path = self.path_from_uri(params["textDocument"]["uri"])
+        source = open(path).read()
+        if len(source.split("\n")[pos["line"]]) < pos["character"]:
+            return {}
+        script = jedi.api.Script(path=path, source=source, line=pos["line"]+1,
+                                 column=pos["character"])
+
+        usages = script.usages()
+        if len(usages) == 0:
+            return {}
+
+        refs = []
+        for u in usages:
+            if u.is_definition():
+                continue
+            refs.append({
+                "uri": "file://" + u.module_path,
+                "range": {
+                    "start": {
+                        "line": u.line-1,
+                        "character": u.column,
+                    },
+                    "end": {
+                        "line": u.line-1,
+                        "character": u.column,
+                    }
+                }
+            })
+        return refs
 
 def main():
-    log("Reading on stdin, writing on stdout")
-    read_messages()
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("--mode", default="stdio", help="communication (stdio|tcp)")
+
+    args = parser.parse_args()
+    rw = None
+    if args.mode == "stdio":
+        log("Reading on stdin, writing on stdout")
+        rw = StdIOReadWriter()
+    elif args.mode == "tcp":
+        log("Accepting TCP connections on {}")
+    server = LangServer(conn=rw)
+    server.serve()
 
 if __name__ == "__main__":
     main()
