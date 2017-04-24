d42d6efd5af309bbec17ce3165953917e2e9c671
Keegan Carruthers-Smith <keegan.csmith@gmail.com>
2017-04-24 18:29:51 +0200

    Remove retry around main
    If we have an exception that crashes the app, then crash the app.

--- python-langserver.py
+++ python-langserver.py
@@ -5,9 +5,4 @@ import time
 import traceback
 import langserver.langserver
 
-while True:
-    try:
-        langserver.langserver.main()
-    except Exception as e:
-        tb = traceback.format_exc()
-        print("FATAL ERROR: {} {}".format(e, tb))
+langserver.langserver.main()
