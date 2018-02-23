c1eb582ece8c3017c2069f08bfe7a4b2d1ab8586
ggilmore <geoffrey@sourcegraph.com>
2018-02-22 18:03:46 -0800

    add stdlib hover test

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -47,3 +47,29 @@ def test_cross_package_hover():
             'Whether or not "fizz" should be printed for this number'
         ]
     }
+
+def test_std_lib_hover():
+    uri = "file:///fizzbuzz_service/__main__.py"
+    line, col = 5, 10
+    result = fizzbuzz_workspace.hover(uri, line, col)
+    assert result == {
+        'contents': [
+            {
+                'language': 'python',
+                'value': 'def print(param value, param ..., param sep, param '
+                                    'end, param file, param flush)'
+            },
+            "print(value, ..., sep=' ', end='\\n', file=sys.stdout, "
+            'flush=False)\n'
+            '\n'
+            'Prints the values to a stream, or to sys.stdout by default.\n'
+            'Optional keyword arguments:\n'
+            'file:  a file-like object (stream); defaults to the current '
+            'sys.stdout.\n'
+            'sep:   string inserted between values, default a space.\n'
+            'end:   string appended after the last value, default a newline.\n'
+            'flush: whether to forcibly flush the stream.'
+        ]
+    }
+
+    
