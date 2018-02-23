a17ed812244a0f91ad9cadac2aa04343a5826a40
ggilmore <geoffrey@sourcegraph.com>
2018-02-23 15:23:13 -0800

    fix indexing std lib when using virtualenv

--- langserver/config.py
+++ langserver/config.py
@@ -1,12 +1,12 @@
 import os
 import os.path
-
+import distutils
 
 class GlobalConfig:
 
     # TODO: allow different Python stdlib versions per workspace?
 
-    PYTHON_PATH = os.path.dirname(os.__file__)
+    PYTHON_PATH = distutils.sysconfig.get_python_lib(standard_lib=True)
     PACKAGES_PARENT = "python-langserver-cache"
     STDLIB_REPO_URL = "git://github.com/python/cpython"
     STDLIB_SRC_PATH = "Lib"
