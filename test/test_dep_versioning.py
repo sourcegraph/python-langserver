827785b9b01e12e0c9ccf7917dbdb657a6b7a6b8
Farhan Attamimi <farhan@sourcegraph.com>
2018-02-26 16:54:37 -0800

    test: add test for NOT fetching correct dependency version

---
+++ test/test_dep_versioning.py
@@ -0,0 +1,33 @@
+from .harness import Harness
+import uuid
+import pytest
+
+
+@pytest.fixture()
+def workspace():
+    workspace = Harness("repos/dep_versioning")
+    workspace.initialize("repos/dep_versioning?" + str(uuid.uuid4()))
+    yield workspace
+    workspace.exit()
+
+
+"""
+This test should pass as long as we do not fetch results from the correct version of dependencies .
+Once we *do*, this test should be updated so the hover returns 'this is version 0.1'.
+"""
+
+
+class TestDependencyVersioning:
+    def test_dep_version(self, workspace):
+        uri = "file:///test.py"
+        character, line = 6, 2
+        result = workspace.hover(uri, line, character)
+        assert result == {
+            'contents': [
+                {
+                    'language': 'python',
+                    'value': 'def testfunc()'
+                },
+                'this is version 0.2'
+            ]
+        }
