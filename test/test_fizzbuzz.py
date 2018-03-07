25304ad1b69f9350d5840b8be07dda973b7783fd
ggilmore <geoffrey@sourcegraph.com>
2018-03-07 14:47:41 -0800

    uncomment hover tests

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -34,73 +34,73 @@ class TestFizzBuzzWorkspace:
     #         "cpython"
     #     }
 
-    # def test_local_hover(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/loopers/number_looper.py"
-    #     line, col = 2, 7
-    #     result = fizzbuzz_workspace.hover(uri, line, col)
-    #     assert result == {
-    #         'contents': [
-    #             {
-    #                 'language': 'python',
-    #                 'value': 'class NumberLooper(param start, param end)'
-    #             },
-    #             'Very important class that is capable of gathering all the number strings '
-    #             'in [start, end)'
-    #         ]
-    #     }
+    def test_local_hover(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/loopers/number_looper.py"
+        line, col = 2, 7
+        result = fizzbuzz_workspace.hover(uri, line, col)
+        assert result == {
+            'contents': [
+                {
+                    'language': 'python',
+                    'value': 'class NumberLooper(param start, param end)'
+                },
+                'Very important class that is capable of gathering all the number strings '
+                'in [start, end)'
+            ]
+        }
 
-    # def test_local_package_cross_module_hover(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/string_deciders/number_decider.py"
-    #     line, col = 4, 22
-    #     result = fizzbuzz_workspace.hover(uri, line, col)
+    def test_local_package_cross_module_hover(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/string_deciders/number_decider.py"
+        line, col = 4, 22
+        result = fizzbuzz_workspace.hover(uri, line, col)
 
-    #     assert result == {
-    #         'contents': [
-    #             {
-    #                 'language': 'python',
-    #                 'value': 'def decide_output_for_number(param number)'
-    #             },
-    #             'Decides the output for a given number'
-    #         ]
-    #     }
+        assert result == {
+            'contents': [
+                {
+                    'language': 'python',
+                    'value': 'def decide_output_for_number(param number)'
+                },
+                'Decides the output for a given number'
+            ]
+        }
 
-    # def test_cross_package_hover(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/checkers/fizzbuzz/fizzbuzz_checker.py"
-    #     line, col = 5, 31
-    #     result = fizzbuzz_workspace.hover(uri, line, col)
-    #     assert result == {
-    #         'contents': [
-    #             {
-    #                 'language': 'python',
-    #                 'value': 'def should_fizz(param number)'
-    #             },
-    #             'Whether or not "fizz" should be printed for this number'
-    #         ]
-    #     }
+    def test_cross_package_hover(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/checkers/fizzbuzz/fizzbuzz_checker.py"
+        line, col = 5, 31
+        result = fizzbuzz_workspace.hover(uri, line, col)
+        assert result == {
+            'contents': [
+                {
+                    'language': 'python',
+                    'value': 'def should_fizz(param number)'
+                },
+                'Whether or not "fizz" should be printed for this number'
+            ]
+        }
 
-    # def test_std_lib_hover(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/__main__.py"
-    #     line, col = 5, 10
-    #     result = fizzbuzz_workspace.hover(uri, line, col)
-    #     assert result == {
-    #         'contents': [
-    #             {
-    #                 'language': 'python',
-    #                 'value': 'def print(param value, param ..., param sep, param '
-    #                 'end, param file, param flush)'
-    #             },
-    #             "print(value, ..., sep=' ', end='\\n', file=sys.stdout, "
-    #             'flush=False)\n'
-    #             '\n'
-    #             'Prints the values to a stream, or to sys.stdout by default.\n'
-    #             'Optional keyword arguments:\n'
-    #             'file:  a file-like object (stream); defaults to the current '
-    #             'sys.stdout.\n'
-    #             'sep:   string inserted between values, default a space.\n'
-    #             'end:   string appended after the last value, default a newline.\n'
-    #             'flush: whether to forcibly flush the stream.'
-    #         ]
-    #     }
+    def test_std_lib_hover(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/__main__.py"
+        line, col = 5, 10
+        result = fizzbuzz_workspace.hover(uri, line, col)
+        assert result == {
+            'contents': [
+                {
+                    'language': 'python',
+                    'value': 'def print(param value, param ..., param sep, param '
+                    'end, param file, param flush)'
+                },
+                "print(value, ..., sep=' ', end='\\n', file=sys.stdout, "
+                'flush=False)\n'
+                '\n'
+                'Prints the values to a stream, or to sys.stdout by default.\n'
+                'Optional keyword arguments:\n'
+                'file:  a file-like object (stream); defaults to the current '
+                'sys.stdout.\n'
+                'sep:   string inserted between values, default a space.\n'
+                'end:   string appended after the last value, default a newline.\n'
+                'flush: whether to forcibly flush the stream.'
+            ]
+        }
 
     # def test_local_defintion(self, fizzbuzz_workspace):
     #     uri = "/fizzbuzz_service/string_deciders/number_decision.py"
