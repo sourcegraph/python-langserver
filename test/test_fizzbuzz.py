f82f8a0710e92d0dd396bde8220f2c7b542ff384
ggilmore <geoffrey@sourcegraph.com>
2018-03-07 15:10:02 -0800

    add local definition tests back for fizzbuzz

--- test/test_fizzbuzz.py
+++ test/test_fizzbuzz.py
@@ -102,99 +102,191 @@ class TestFizzBuzzWorkspace:
             ]
         }
 
-    # def test_local_defintion(self, fizzbuzz_workspace):
-    #     uri = "/fizzbuzz_service/string_deciders/number_decision.py"
-    #     line, col = 21, 21
-    #     result = fizzbuzz_workspace.definition(uri, line, col)
-    #     assert result == [
-    #         {
-    #             'symbol': {
-    #                 'package': {
-    #                     'name': 'fizzbuzz_service'
-    #                 },
-    #                 'name': 'OutputDecision',
-    #                 'container': 'fizzbuzz_service.string_deciders.number_decision',
-    #                 'kind': 'class',
-    #                 'file': 'number_decision.py',
-    #                 'position': {
-    #                     'line': 5,
-    #                     'character': 6
-    #                 }
-    #             },
-    #             'location': {
-    #                 'uri': 'file:///fizzbuzz_service/string_deciders/number_decision.py',
-    #                 'range': {
-    #                     'start': {
-    #                         'line': 5,
-    #                         'character': 6
-    #                     },
-    #                     'end': {
-    #                         'line': 5,
-    #                         'character': 20
-    #                     }
-    #                 }
-    #             }
-    #         }
-    #     ]
+    def test_local_defintion(self, fizzbuzz_workspace):
+        uri = "/fizzbuzz_service/string_deciders/number_decision.py"
+        line, col = 21, 21
+        result = fizzbuzz_workspace.definition(uri, line, col)
 
-    # def test_local_package_cross_module_definition(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/string_deciders/number_decider.py"
-    #     line, col = 4, 25
+        assert len(result) == 1
+        definition = result[0]
+
+        assert "location" in definition
+        assert definition["location"] == {
+            'uri': 'file:///fizzbuzz_service/string_deciders/number_decision.py',
+            'range': {
+                'start': {
+                    'line': 5,
+                    'character': 6
+                },
+                'end': {
+                    'line': 5,
+                    'character': 20
+                }
+            }
+        }
+
+    def test_local_package_cross_module_definition(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/string_deciders/number_decider.py"
+        line, col = 4, 25
+        result = fizzbuzz_workspace.definition(uri, line, col)
+
+        assert len(result) == 2
+        assert all(["location" in d for d in result])
+
+        result_locations = [d["location"] for d in result]
+
+        definition_location = {
+            'uri': 'file:///fizzbuzz_service/string_deciders/number_decision.py',
+            'range': {
+                'start': {
+                    'line': 12,
+                    'character': 4
+                },
+                'end': {
+                    'line': 12,
+                    'character': 28
+                }
+            }
+        }
+
+        # from the import statement at the top of the file
+        assignment_location = {
+            'uri': 'file:///fizzbuzz_service/string_deciders/number_decider.py',
+            'range': {
+                'start': {
+                    'line': 0,
+                    'character': 45
+                },
+                'end': {
+                    'line': 0,
+                    'character': 69
+                }
+            }
+        }
+
+        assert definition_location in result_locations
+        assert assignment_location in result_locations
+
+    def test_cross_package_definition(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/checkers/fizzbuzz/fizzbuzz_checker.py"
+        line, col = 5, 57
+        result = fizzbuzz_workspace.definition(uri, line, col)
+
+        assert len(result) == 2
+        assert all(["location" in d for d in result])
+
+        result_locations = [d["location"] for d in result]
+
+        definition_location = {
+            'uri': 'file:///fizzbuzz_service/checkers/buzz/buzz_checker.py',
+            'range': {
+                'start': {
+                    'line': 0,
+                    'character': 4
+                },
+                'end': {
+                    'line': 0,
+                    'character': 15
+                }
+            }
+        }
+
+        # from the import statement at the top of the file
+        assignment_location = {
+            'uri': 'file:///fizzbuzz_service/checkers/fizzbuzz/fizzbuzz_checker.py',
+            'range': {
+                'start': {
+                    'line': 1,
+                    'character': 32
+                },
+                'end': {
+                    'line': 1,
+                    'character': 43
+                }
+            }
+        }
+
+        assert definition_location in result_locations
+        assert assignment_location in result_locations
+
+    def test_local_package_cross_module_import_definition(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/string_deciders/number_decider.py"
+        line, col = 0, 14
+        result = fizzbuzz_workspace.definition(uri, line, col)
+
+        assert len(result) == 1
+        definition = result[0]
+
+        assert "location" in definition
+        location = definition["location"]
+        assert "uri" in location
+        assert location["uri"] == 'file:///fizzbuzz_service/string_deciders/number_decision.py'
+
+        # TODO: In the case of a module, does the range have any meaning?
+
+    def test_cross_package_import_definition(self, fizzbuzz_workspace):
+        uri = "file:///fizzbuzz_service/loopers/number_looper.py"
+        line, col = 0, 31
+        result = fizzbuzz_workspace.definition(uri, line, col)
+
+        assert len(result) == 1
+        definition = result[0]
+
+        assert "location" in definition
+        location = definition["location"]
+        assert "uri" in location
+        assert location["uri"] == 'file:///fizzbuzz_service/string_deciders/__init__.py'
+
+        # TODO: In the case of a module, does the range have any meaning?
+
+    # def test_std_lib_definition(self, fizzbuzz_workspace):
+    #     uri = 'file:///fizzbuzz_service/string_deciders/number_decision.py'
+    #     line, col = 5, 23
     #     result = fizzbuzz_workspace.definition(uri, line, col)
+
     #     definition = {
+    #         'symbol': {
+    #             'package': {
+    #                 'name': 'cpython'
+    #             },
+    #             'name': 'Enum',
+    #             'container': 'enum',
+    #             'kind': 'class',
+    #             'path': 'Lib/enum.py',
+    #             'file': 'enum.py',
+    #             'position': {
+    #                 'line': 508,
+    #                 'character': 6
+    #             },
+    #         },
+    #         'location': None
+    #     }
+
+    #     # from the import statement at the top of the file
+    #     assignment = {
     #         'symbol': {
     #             'package': {
     #                 'name': 'fizzbuzz_service'
     #             },
-    #             'name': 'decide_output_for_number',
+    #             'name': 'Enum',
     #             'container': 'fizzbuzz_service.string_deciders.number_decision',
-    #             'kind': 'def',
+    #             'kind': 'class',
     #             'file': 'number_decision.py',
     #             'position': {
-    #                     'line': 12,
-    #                     'character': 4
-    #             }
+    #                 'line': 0,
+    #                 'character': 17
+    #             },
     #         },
     #         'location': {
     #             'uri': 'file:///fizzbuzz_service/string_deciders/number_decision.py',
     #             'range': {
     #                 'start': {
-    #                     'line': 12,
-    #                     'character': 4
-    #                 },
-    #                 'end': {
-    #                     'line': 12,
-    #                     'character': 28
-    #                 }
-    #             }
-    #         }
-    #     }
-
-    #     # from the import statement at the top of the file
-    #     assignment = {
-    #         'symbol': {
-    #             'package': {
-    #                 'name': 'fizzbuzz_service'
-    #             },
-    #             'name': 'decide_output_for_number',
-    #             'container': 'fizzbuzz_service.string_deciders.number_decider',
-    #             'kind': 'def',
-    #             'file': 'number_decider.py',
-    #             'position': {
-    #                 'line': 0,
-    #                 'character': 45
-    #             }
-    #         },
-    #         'location': {
-    #             'uri': 'file:///fizzbuzz_service/string_deciders/number_decider.py',
-    #             'range': {
-    #                 'start': {
     #                     'line': 0,
-    #                     'character': 45
+    #                     'character': 17
     #                 },
     #                 'end': {
     #                     'line': 0,
-    #                     'character': 69
+    #                     'character': 21
     #                 }
     #             }
     #         }
@@ -204,193 +296,6 @@ class TestFizzBuzzWorkspace:
 
     #     assert definition in result
     #     assert assignment in result
-
-    # def test_cross_package_definition(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/checkers/fizzbuzz/fizzbuzz_checker.py"
-    #     line, col = 5, 57
-    #     result = fizzbuzz_workspace.definition(uri, line, col)
-
-    #     definition = {
-    #         'symbol': {
-    #             'package': {
-    #                 'name': 'fizzbuzz_service'
-    #             },
-    #             'name': 'should_buzz',
-    #             'container': 'buzz.buzz_checker',
-    #             'kind': 'def',
-    #             'file': 'buzz_checker.py',
-    #             'position': {
-    #                 'line': 0,
-    #                 'character': 4
-    #             }
-    #         },
-    #         'location': {
-    #             'uri': 'file:///fizzbuzz_service/checkers/buzz/buzz_checker.py',
-    #             'range': {
-    #                 'start': {
-    #                     'line': 0,
-    #                     'character': 4
-    #                 },
-    #                 'end': {
-    #                     'line': 0,
-    #                     'character': 15
-    #                 }
-    #             }
-    #         }
-    #     }
-
-    #     # from the import statement at the top of the file
-    #     assignment = {
-    #         'symbol': {
-    #             'package': {
-    #                 'name': 'fizzbuzz_service'
-    #             },
-    #             'name': 'should_buzz',
-    #             'container': 'fizzbuzz_service.checkers.fizzbuzz.fizzbuzz_checker',
-    #             'kind': 'def',
-    #             'file': 'fizzbuzz_checker.py',
-    #             'position': {
-    #                 'line': 1,
-    #                 'character': 32
-    #             },
-    #         },
-    #         'location': {
-    #             'uri': 'file:///fizzbuzz_service/checkers/fizzbuzz/fizzbuzz_checker.py',
-    #             'range': {
-    #                 'start': {
-    #                     'line': 1,
-    #                     'character': 32
-    #                 },
-    #                 'end': {
-    #                     'line': 1,
-    #                     'character': 43
-    #                 }
-    #             }
-    #         }
-    #     }
-
-    #     assert len(result) == 2
-
-    #     assert definition in result
-    #     assert assignment in result
-
-    # def test_local_package_cross_module_import_definition(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/string_deciders/number_decider.py"
-    #     line, col = 0, 14
-    #     result = fizzbuzz_workspace.definition(uri, line, col)
-
-    #     assert len(result) == 1
-    #     definition = result[0]
-
-    #     assert "symbol" in definition
-    #     assert definition["symbol"] == {
-    #         'package': {
-    #             'name': 'fizzbuzz_service'},
-    #         'name': 'number_decision',
-    #         'container': 'fizzbuzz_service.string_deciders.number_decision',
-    #         'kind': 'module',
-    #         'file': 'number_decision.py',
-    #         'position': {
-    #             'line': 0,
-    #             'character': 0},
-    #     }
-
-    #     assert "location" in definition
-    #     location = definition["location"]
-    #     assert "uri" in location
-    #     assert location["uri"] == 'file:///fizzbuzz_service/string_deciders/number_decision.py'
-
-    #     # TODO: In the case of a module, does the range have any meaning?
-
-    # def test_cross_package_import_definition(self, fizzbuzz_workspace):
-    #     uri = "file:///fizzbuzz_service/loopers/number_looper.py"
-    #     line, col = 0, 31
-    #     result = fizzbuzz_workspace.definition(uri, line, col)
-
-    #     assert len(result) == 1
-    #     definition = result[0]
-
-    #     assert "symbol" in definition
-    #     assert definition["symbol"] == {
-    #         'package': {
-    #             'name': 'fizzbuzz_service'
-    #         },
-    #         'name': 'string_deciders',
-    #         'container': 'fizzbuzz_service.string_deciders',
-    #         'kind': 'module',
-    #         'file': '__init__.py',
-    #         'position': {
-    #             'line': 0,
-    #             'character': 0
-    #         },
-    #     }
-
-    #     assert "location" in definition
-    #     location = definition["location"]
-    #     assert "uri" in location
-    #     assert location["uri"] == 'file:///fizzbuzz_service/string_deciders/__init__.py'
-
-    #     # TODO: In the case of a module, does the range have any meaning?
-
-    def test_std_lib_definition(self, fizzbuzz_workspace):
-        uri = 'file:///fizzbuzz_service/string_deciders/number_decision.py'
-        line, col = 5, 23
-        result = fizzbuzz_workspace.definition(uri, line, col)
-
-        definition = {
-            'symbol': {
-                'package': {
-                    'name': 'cpython'
-                },
-                'name': 'Enum',
-                'container': 'enum',
-                'kind': 'class',
-                'path': 'Lib/enum.py',
-                'file': 'enum.py',
-                'position': {
-                    'line': 508,
-                    'character': 6
-                },
-            },
-            'location': None
-        }
-
-        # from the import statement at the top of the file
-        assignment = {
-            'symbol': {
-                'package': {
-                    'name': 'fizzbuzz_service'
-                },
-                'name': 'Enum',
-                'container': 'fizzbuzz_service.string_deciders.number_decision',
-                'kind': 'class',
-                'file': 'number_decision.py',
-                'position': {
-                    'line': 0,
-                    'character': 17
-                },
-            },
-            'location': {
-                'uri': 'file:///fizzbuzz_service/string_deciders/number_decision.py',
-                'range': {
-                    'start': {
-                        'line': 0,
-                        'character': 17
-                    },
-                    'end': {
-                        'line': 0,
-                        'character': 21
-                    }
-                }
-            }
-        }
-
-        import pdb
-        pdb.set_trace()
-        assert len(result) == 2
-
-        assert definition in result
-        assert assignment in result
 
     # def test_local_references(self, fizzbuzz_workspace):
     #     uri = 'file:///fizzbuzz_service/string_deciders/number_decision.py'
